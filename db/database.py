82e3a3d454020f022fd50cbedaae969fd1a0ce0c
Илья Снимщиков <igsnimschikov@edu.hse.ru>
2023-10-12 06:50:32 +0300

    fix: add missing param

--- db/database.py
+++ db/database.py
@@ -21,4 +21,4 @@ async def close():
 async def migrate():
     command = Command(tortoise_config=TORTOISE_ORM)
     await command.init()
-    await command.upgrade()
+    await command.upgrade(run_in_transaction=True)
